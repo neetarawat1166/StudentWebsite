5ba34ed54c7a864c02af012db366896e056375d5
[neeta rawat] <[neetarawat1166@gmail.com]>
2024-04-05 10:06:07 +0530

    Routing done

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Route,RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './pages/Home/Home.jsx'
+import About from './pages/About/About.jsx'
+import Contact from './pages/Contact/Contact.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path ='/' element={<Layout/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Route>
+  )
+);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
-)
+);
