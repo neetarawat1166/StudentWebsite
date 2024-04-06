55d6c2e52809b8ad4e99bc9f3ebf29dda5e98111
Kirtika Chaudhary <kirtikachaudhary1815@gmail.com>
2024-04-06 12:57:20 +0530

    HomeBrowseCategory Update

---
+++ Frontend/src/components/common/BrowseCategoryCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const BrowseCategoryCard = ({ icon ,categoryName, categoryDesc}) => {
+  const Icon = icon;
+
+  return (
+    <div className="group bg-[#eeeeee] mx-auto lg:justify-between rounded-lg pt-10 px-5 w-[300px] md:w-[350px] lg:w-[500px] xl:w-[300px] text-center hover:bg-[#65bc7b] transition-colors duration-300 ease-in-out">
+      <div className="rounded-full w-[100px] h-[100px] flex items-center justify-center mx-auto border-2 border-dashed border-[#65bc7b] group-hover:border-white">
+      <div className="bg-[#65bc7b] rounded-full w-[90px] h-[90px] flex items-center justify-center mx-auto group-hover:bg-white">
+        <Icon className="text-white text-[50px] group-hover:text-[#65bc7b]" />
+      </div>
+      </div>
+      <div className="pt-6 pb-5">
+        <p className="text-xl font-semibold group-hover:text-white">{categoryName}</p>
+        <p className='pt-4 pb-8 group-hover:text-white'>{categoryDesc}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BrowseCategoryCard;
--- Frontend/src/components/section/HomePage/HomeBrowseCategory.jsx
+++ Frontend/src/components/section/HomePage/HomeBrowseCategory.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import BrowseCategoryCard from '../../common/BrowseCategoryCard';
+import browseCategoryImg1 from '../../../images/browseCategoryImg1.png';
+import { IoNewspaper } from "react-icons/io5";
 
 const HomeBrowseCategory = () => {
-  return (
-    <div>HomeBrowseCategory</div>
-  )
-}
+  // Sample data for categories
+  const categories = [
+    {
+      name: 'Category 1',
+      icon: IoNewspaper,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing amet consectetur adipisicing elit. Dolorum, facilis!"
+    },
+    {
+      name: 'Category 2',
+      icon: IoNewspaper,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing amet consectetur adipisicing elit. Dolorum, facilis!"
+    },
+    {
+      name: 'Category 3',
+      icon: IoNewspaper,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing amet consectetur adipisicing elit. Dolorum, facilis!"
+    },
+    {
+      name: 'Category 4',
+      icon: IoNewspaper,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing amet consectetur adipisicing elit. Dolorum, facilis!"
+    }
+  ];
 
-export default HomeBrowseCategory
+  return (
+    <div className="px-5 py-5 md:px-10 md:py-10 lg:px-2 lg:py-20">
+      <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-10 text-center">Browse By Categories</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-6 md:gap-10">
+        {categories.map((category, index) => (
+          <BrowseCategoryCard
+            key={index}
+            categoryName={category.name}
+            icon={category.icon}
+            categoryDesc={category.desc}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default HomeBrowseCategory;
