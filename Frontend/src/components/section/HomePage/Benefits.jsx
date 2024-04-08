752d2b2346af5760937f8ab16de47d6a999c485c
[neeta rawat] <[neetarawat1166@gmail.com]>
2024-04-08 09:56:19 +0530

    CTA Done

--- Frontend/src/components/section/HomePage/Benefits.jsx
+++ Frontend/src/components/section/HomePage/Benefits.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import img1 from '../../../images/benedes1.png'
+import { HiCheckBadge } from "react-icons/hi2";
 
 const Benefits = () => {
   return (
@@ -10,8 +11,8 @@ const Benefits = () => {
               <img src={img1} alt="Benefits" />
           </div>
           <div className='container'>
-            <div className='grid items-center'>
-              <div className='grid-cols-6 mb-[30px]'>
+            <div className='grid grid-cols-2 items-center'>
+              <div className='mb-[30px]'>
                   <div>
                       <div className='mb-[30px]'>
                         <span className='inline-block mb-[25px] bg-[#65bc7b] rounded-lg p-2'>WHY CHOOSE US</span>
@@ -21,8 +22,24 @@ const Benefits = () => {
                         </h4>
                       </div>
                       {/* start from here */}
-                      <div className=''>
-
+                      <div className='pb-[15px]'>
+                        <p>
+                          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime aliquid, dolorem iure molestiae dolores voluptatibus sit voluptatum architecto similique adipisci.
+                        </p>
+                      </div>
+                      <div className='grid grid-cols-2 gap-4'>
+                        <div className='mb-[20px]'>
+                            <div className='rounded-sm p-5 bg-[#eeeeee]'>
+                            <HiCheckBadge /><span>World class trainers</span>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente neque dignissimos commodi laborum? Sed cum, eius eaque repudiandae est delectus?</p>
+                            </div>
+                        </div>
+                        <div className='mb-[20px]'>
+                            <div className='rounded-sm p-5 bg-[#eeeeee]'>
+                            <HiCheckBadge /><span>World class trainers</span>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente neque dignissimos commodi laborum? Sed cum, eius eaque repudiandae est delectus?</p>
+                            </div>
+                        </div>
                       </div>
                   </div>
               </div>
