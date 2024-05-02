fe5c5632ad8fb14ce9215e3e0568e4c2279c3281
[neeta rawat] <[neetarawat1166@gmail.com]>
2024-05-02 15:31:17 +0530

    New tabs added

--- Frontend/src/pages/Dashboard/Dashboard.jsx
+++ Frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,56 +1,3 @@
-// import React from "react";
-// import Profile from "../../components/common/Profile";
-// import Timetable from "../../components/common/Timetable";
-// import Attendance from "../../components/common/Attendance";
-// import Announcement from "../../components/common/Announcement";
-// import TeacherProfile from "../../components/common/TeacherProfile";
-
-// const Dashboard = () => {
-
-//     const announcements = [
-//         {
-//             heading: "Heading 1",
-//             content: ["Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, cum.", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, cum.", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, cum.","Lorem ipsum dolor, sit amet consectetur adipisicing elit. Distinctio, cum."]
-//           },
-//           {
-//             heading: "Heading 2",
-//             content: ["Content 4", "Content 5"]
-//           }
-//       ];
-
-//   return (
-//     <>
-//       <section className="pr-4">
-//         <div className="flex">
-//           <div className="hidden lg:block w-[20%] ">
-//             <Profile />
-//           </div>
-//           <div className="w-[80%] ">
-//             <div className="">
-//               <div className="">
-//                 <Timetable/>
-//               </div>
-//             </div>
-//             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 pl-4">
-//               <div className=" ">
-//               <Attendance attended="28" total="30"/>
-//               </div>
-//               <div className="">
-//               <Announcement announcements={announcements}/>  
-//               </div>
-//             </div>
-//             <div className="md:col-span-1 pl-4">
-//                 <TeacherProfile/>   
-//             </div>
-//           </div>
-//         </div>
-//       </section>
-//     </>
-//   );
-// };
-
-// export default Dashboard;
-
 import React from "react";
 import Profile from "../../components/common/Profile";
 import Timetable from "../../components/common/Timetable";
@@ -75,7 +22,7 @@ const Dashboard = () => {
     <>
       <section className="pr-4 relative">
         <div className="flex">
-          <div className="hidden lg:block w-[20%] h-auto bg-[#eeeeee]">
+          <div className="hidden lg:block w-[20%] h-auto bg-[#eeeeee]">``
             <div className=" sticky top-0">
               <Profile />
             </div>
