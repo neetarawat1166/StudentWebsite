a8310e454ef4899f0af9419d255d1deade75ec60
[neeta rawat] <[neetarawat1166@gmail.com]>
2024-05-02 15:53:05 +0530

    Header changes done

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -14,8 +14,12 @@ import Timetable from './pages/Timetable/Timetable.jsx'
 import Query from './pages/Query/Query.jsx'
 import Feedback from './pages/Feedback/Feedback.jsx'
 import { AuthProvider } from './pages/Signup/AuthContext.jsx'
-import Resources from '../src/teacherPages/Resources/Resources.jsx'
-import Syllabus from '../src/teacherPages/Syllabus/Syllabus.jsx'
+import Resources from '../src/pages/Resources/Resources.jsx'
+import Syllabus from '../src/pages/Syllabus/Syllabus.jsx'
+import TeacherAssignment from '../src/teacherPages/TeacherAssignment/TeacherAssignment.jsx'
+import TeacherResources from '../src/teacherPages/TeacherResources/TeacherResources.jsx'
+import TeacherSyllabus from '../src/teacherPages/TeacherSyllabus/TeacherSyllabus.jsx'
+import TeacherDashboard from '../src/teacherPages/TeacherDashboard/TeacherDashboard.jsx'
 // import ProtectedRoute from '../../Frontend/src/pages/Dashboard/ProtectedRoute.jsx'; // Import the ProtectedRoute component
 
 const router = createBrowserRouter(
@@ -27,9 +31,13 @@ const router = createBrowserRouter(
       <Route  path="/signup" element={<Signup/>} />
       <Route path='/login' element={<Login/>}/>
       <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/dashboard-1' element={<TeacherDashboard/>}/>
       <Route path='/assignment' element={<Assignment/>}/>
+      <Route path='/assignment-1' element={<TeacherAssignment/>}/>
       <Route path='/resources' element={<Resources/>}/>
+      <Route path='/resources-1' element={<TeacherResources/>}/>
       <Route path='/syllabus' element={<Syllabus/>}/>
+      <Route path='/syllabus-1' element={<TeacherSyllabus/>}/>
       <Route path='/timetable' element={<Timetable/>}/>
       <Route path='/query' element={<Query/>}/>
       <Route path='/feedback' element={<Feedback/>}/>
