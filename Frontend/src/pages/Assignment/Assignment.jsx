348fd0c9301a427851832927ed55011f99c8f97e
[neeta rawat] <[neetarawat1166@gmail.com]>
2024-05-02 16:31:47 +0530

    changes done

--- Frontend/src/pages/Assignment/Assignment.jsx
+++ Frontend/src/pages/Assignment/Assignment.jsx
@@ -6,26 +6,26 @@ const Assignment = () => {
     {
       title: "Building a Full-Stack Application",
       task: "You are tasked with developing a full-stack application for managing a library system. The application should allow users to browse, borrow, and return books, as well as manage their accounts.",
-      assignDate: "04/19/2024",
-      dueDate: "04/30/2024",
+      assignDate: "05/02/2024",
+      dueDate: "05/30/2024",
     },
     {
       title: "Building a Social Media Platform",
       task: "Your task is to develop a full-stack social media platform that allows users to create profiles, connect with friends, share posts, and interact with each other.",
-      assignDate: "04/16/2024",
-      dueDate: "04/30/2024",  
+      assignDate: "05/02/2024",
+      dueDate: "05/30/2024",  
     },   
     {
       title: "E-commerce Website Development",
       task: "Your task is to develop a full-stack e-commerce website where users can browse products, add them to their cart, proceed to checkout, and make purchases.",
       assignDate: "04/19/2024",
-      dueDate: "04/30/2024",
+      dueDate: "05/30/2024",
     },
     {
       title: "Task Management System",
       task: "Your task is to develop a full-stack task management system where users can create, assign, prioritize, and track tasks within a team or organization.",
       assignDate: "04/15/2024",
-      dueDate: "04/30/2024",
+      dueDate: "05/30/2024",
     },
     // Add more assignments as needed
   ];
